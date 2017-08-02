d9d84575d4663f718ea0ed0c1168b01a2d03d2f0
Dave Try <dave.try@puppet.com>
2017-08-02 17:33:56 +1000

    fixing tabs

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class helm::params {
-	$version      = undef
-	$install_path = '/usr/bin'
+  $version      = undef
+  $install_path = '/usr/bin'
   $init = true
   $service_account = 'tiller'
   $tiller_namespace = 'kube-system'
