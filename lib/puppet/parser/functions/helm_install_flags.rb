f9518ab2d17d70afe88e2e0a4f07238e6ece26c2
sheenaajay <sheena@puppet.com>
2018-01-15 20:42:08 +0000

    (CLOUD-1602) Fix rubocop error updategemsfori18n (#29)

--- lib/puppet/parser/functions/helm_install_flags.rb
+++ lib/puppet/parser/functions/helm_install_flags.rb
@@ -1,74 +1,28 @@
 require 'shellwords'
-
+#
+# helm_install_flags.rb
+#
 module Puppet::Parser::Functions
   # Transforms a hash into a string of helm install chart flags
   newfunction(:helm_install_flags, :type => :rvalue) do |args|
     opts = args[0] || {}
     flags = []
-
-    if opts['ensure'].to_s == 'present'
-      flags << "install"
-    end
-
-    if opts['ca_file'].to_s != 'undef'
-      flags << "--ca-file '#{opts['ca_file']}'"
-    end
-
-    if opts['cert_file'].to_s != 'undef'
-      flags << "--cert-file '#{opts['cert_file']}'"
-    end
-
-    if opts['debug']
-      flags << '--debug'
-    end
-
-    if opts['devel']
-      flags << '--devel'
-    end
-
-    if opts['dry_run']
-      flags << '--dry_run'
-    end
-
-    if opts['key_file'].to_s != 'undef'
-      flags << "--key-file '#{opts['key_file']}'"
-    end
-
-    if opts['key_ring'].to_s != 'undef'
-      flags << "--key-ring '#{opts['key_ring']}'"
-    end
-
-    if opts['home'].to_s != 'undef'
-      flags << "--home '#{opts['home']}'"
-    end
-
-    if opts['host'].to_s != 'undef'
-      flags << "--host '#{opts['host']}'"
-    end
-
-    if opts['kube_context'].to_s != 'undef'
-      flags << "--kube-context '#{opts['kube_context']}'"
-    end
-
-    if opts['release_name'].to_s != 'undef'
-      flags << "--name '#{opts['release_name']}'"
-    end
-
-    if opts['name_template'].to_s != 'undef'
-      flags << "--name-template '#{opts['name_template']}'"
-    end
-
-    if opts['namespace'].to_s != 'undef'
-      flags << "--namespace '#{opts['namespace']}'"
-    end
-
-    if opts['no_hooks']
-      flags << '--no-hooks'
-    end
-
-    if opts['replace']
-      flags << '--replace'
-    end
+    flags << 'install' if opts['ensure'].to_s == 'present'
+    flags << "--ca-file '#{opts['ca_file']}'" if opts['ca_file'].to_s != 'undef'
+    flags << "--cert-file '#{opts['cert_file']}'" if opts['cert_file'].to_s != 'undef'
+    flags << '--debug' if opts['debug']
+    flags << '--devel' if opts['devel']
+    flags << '--dry_run' if opts['dry_run']
+    flags << "--key-file '#{opts['key_file']}'" if opts['key_file'].to_s != 'undef'
+    flags << "--key-ring '#{opts['key_ring']}'" if opts['key_ring'].to_s != 'undef'
+    flags << "--home '#{opts['home']}'" if opts['home'].to_s != 'undef'
+    flags << "--host '#{opts['host']}'" if opts['host'].to_s != 'undef'
+    flags << "--kube-context '#{opts['kube_context']}'" if opts['kube_context'].to_s != 'undef'
+    flags << "--name '#{opts['release_name']}'" if opts['release_name'].to_s != 'undef'
+    flags << "--name-template '#{opts['name_template']}'" if opts['name_template'].to_s != 'undef'
+    flags << "--namespace '#{opts['namespace']}'" if opts['namespace'].to_s != 'undef'
+    flags << '--no-hooks' if opts['no_hooks']
+    flags << '--replace' if opts['replace']
 
     if opts['repo'].to_s != 'undef'
       flags << "--repo '#{opts['repo']}'"
@@ -81,57 +35,24 @@ module Puppet::Parser::Functions
 
     [
       ['--set %s',  'set'],
-      [' --values %s', 'values']
+      [' --values %s', 'values'],
     ].each do |(format, key)|
       values    = opts[key]
       new_flags = multi_flags.call(values, format)
       flags.concat(new_flags)
     end
 
-    if opts['timeout'].to_s != 'undef'
-      flags << "--timeout '#{opts['timeout']}'"
-    end
-
-    if opts['tiller_namespace'].to_s != 'undef'
-      flags << "--tiller-namespace '#{opts['tiller_namespace']}'"
-    end
-
-    if opts['tls']
-      flags << "--tls"
-    end
-
-    if opts['tls_ca_cert'].to_s != 'undef'
-      flags << "--tls-ca-cert '#{opts['tls_ca_cert']}'"
-    end
-
-    if opts['tls_cert'].to_s != 'undef'
-      flags << "--tls-cert '#{opts['tls_cert']}'"
-    end
-
-     if opts['tls_key'].to_s != 'undef'
-      flags << "--tls-key '#{opts['tls_key']}'"
-    end
-
-    if opts['tls_verify']
-      flags << '--tls-verify'
-    end
-
-    if opts['verify']
-      flags << '--verify'
-    end
-
-    if opts['version'].to_s != 'undef'
-      flags << "--version '#{opts['version']}'"
-    end
-
-    if opts['wait']
-      flags << '--wait'
-    end
-
-    if opts['chart'].to_s != 'undef'
-      flags << "'#{opts['chart']}'"
-    end
-
-   flags.flatten.join(" ")
+    flags << "--timeout '#{opts['timeout']}'" if opts['timeout'].to_s != 'undef'
+    flags << "--tiller-namespace '#{opts['tiller_namespace']}'" if opts['tiller_namespace'].to_s != 'undef'
+    flags << '--tls' if opts['tls']
+    flags << "--tls-ca-cert '#{opts['tls_ca_cert']}'" if opts['tls_ca_cert'].to_s != 'undef'
+    flags << "--tls-cert '#{opts['tls_cert']}'" if opts['tls_cert'].to_s != 'undef'
+    flags << "--tls-key '#{opts['tls_key']}'" if opts['tls_key'].to_s != 'undef'
+    flags << '--tls-verify' if opts['tls_verify']
+    flags << '--verify' if opts['verify']
+    flags << "--version '#{opts['version']}'" if opts['version'].to_s != 'undef'
+    flags << '--wait' if opts['wait']
+    flags << "'#{opts['chart']}'" if opts['chart'].to_s != 'undef'
+    flags.flatten.join(' ')
   end
 end
--- lib/puppet/parser/functions/helm_ls_flags.rb
+++ lib/puppet/parser/functions/helm_ls_flags.rb
@@ -1,54 +1,23 @@
 require 'shellwords'
-
+#
+# helm_ls_flags.rb
+#
 module Puppet::Parser::Functions
   # Transforms a hash into a string of helm ls flags
   newfunction(:helm_ls_flags, :type => :rvalue) do |args|
     opts = args[0] || {}
     flags = []
-
-    if opts['ls']
-      flags << 'ls'
-    end
-
-    if opts['home'].to_s != 'undef'
-      flags << "--home '#{opts['home']}'"
-    end
-
-    if opts['host'].to_s != 'undef'
-      flags << "--host '#{opts['host']}'"
-    end
-
-    if opts['kube_context'].to_s != 'undef'
-      flags << "--kube-context '#{opts['kube_context']}'"
-    end
-    if opts['tiller_namespace'].to_s != 'undef'
-      flags << "--tiller-namespace '#{opts['tiller_namespace']}'"
-    end
-
-    if opts['short']
-      flags << "--short"
-    end
-
-    if opts['tls']
-      flags << "--tls"
-    end
-
-    if opts['tls_ca_cert'].to_s != 'undef'
-      flags << "--tls-ca-cert '#{opts['tls_ca_cert']}'"
-    end
-
-    if opts['tls_cert'].to_s != 'undef'
-      flags << "--tls-cert '#{opts['tls_cert']}'"
-    end
-
-     if opts['tls_key'].to_s != 'undef'
-      flags << "--tls-key '#{opts['tls_key']}'"
-    end
-
-    if opts['tls_verify']
-      flags << '--tls-verify'
-    end
-
-   flags.flatten.join(" ")
+    flags << 'ls' if opts['ls']
+    flags << "--home '#{opts['home']}'" if opts['home'].to_s != 'undef'
+    flags << "--host '#{opts['host']}'" if opts['host'].to_s != 'undef'
+    flags << "--kube-context '#{opts['kube_context']}'" if opts['kube_context'].to_s != 'undef'
+    flags << "--tiller-namespace '#{opts['tiller_namespace']}'" if opts['tiller_namespace'].to_s != 'undef'
+    flags << '--short' if opts['short']
+    flags << '--tls' if opts['tls']
+    flags << "--tls-ca-cert '#{opts['tls_ca_cert']}'" if opts['tls_ca_cert'].to_s != 'undef'
+    flags << "--tls-cert '#{opts['tls_cert']}'" if opts['tls_cert'].to_s != 'undef'
+    flags << "--tls-key '#{opts['tls_key']}'" if opts['tls_key'].to_s != 'undef'
+    flags << '--tls-verify' if opts['tls_verify']
+    flags.flatten.join(' ')
   end
 end
