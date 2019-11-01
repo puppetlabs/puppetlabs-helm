12dc67672f806e1fdbbed7b89d9a11ebb2540983
Nicko Glayre <nicko.glayre@kudelskisecurity.com>
2019-11-01 09:28:19 +0100

    Adding the possibility to add imagePullSecrets to the tiller ServiceAccount

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,32 +1,33 @@
 # == Class: helm::params
 class helm::params {
-  $canary_image         = false
-  $client_only          = false
-  $debug                = false
-  $dry_run              = false
-  $env                  = [ 'HOME=/root', 'KUBECONFIG=/etc/kubernetes/admin.conf']
-  $home                 = undef
-  $host                 = undef
-  $init                 = true
-  $install_path         = '/usr/bin'
-  $kube_context         = undef
-  $local_repo_url       = undef
-  $proxy                = undef
-  $net_host             = false
-  $node_selectors       = undef
-  $overrides            = undef
-  $path                 = [ '/bin','/usr/bin']
-  $service_account      = 'tiller'
-  $skip_refresh         = false
-  $stable_repo_url      = undef
-  $tiller_image         = undef
-  $tiller_namespaces    = ['kube-system']
-  $tiller_tls           = false
-  $tiller_tls_cert      = undef
-  $tiller_tls_key       = undef
-  $tiller_tls_verify    = false
-  $tls_ca_cert          = undef
-  $upgrade              = false
-  $version              = '2.7.2'
-  $archive_baseurl      = 'https://kubernetes-helm.storage.googleapis.com'
+  $canary_image              = false
+  $client_only               = false
+  $debug                     = false
+  $dry_run                   = false
+  $env                       = [ 'HOME=/root', 'KUBECONFIG=/etc/kubernetes/admin.conf']
+  $home                      = undef
+  $host                      = undef
+  $init                      = true
+  $install_path              = '/usr/bin'
+  $kube_context              = undef
+  $local_repo_url            = undef
+  $proxy                     = undef
+  $net_host                  = false
+  $node_selectors            = undef
+  $overrides                 = undef
+  $path                      = [ '/bin','/usr/bin']
+  $service_account           = 'tiller'
+  $skip_refresh              = false
+  $stable_repo_url           = undef
+  $tiller_image              = undef
+  $tiller_image_pull_secrets = undef
+  $tiller_namespaces         = ['kube-system']
+  $tiller_tls                = false
+  $tiller_tls_cert           = undef
+  $tiller_tls_key            = undef
+  $tiller_tls_verify         = false
+  $tls_ca_cert               = undef
+  $upgrade                   = false
+  $version                   = '2.7.2'
+  $archive_baseurl           = 'https://kubernetes-helm.storage.googleapis.com'
 }
