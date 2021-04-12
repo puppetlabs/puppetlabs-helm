defb0741ee0e8027b1fbf6d2b12d4177528a4620
daianamezdrea <46529728+daianamezdrea@users.noreply.github.com>
2021-04-12 14:05:39 +0300

    Update puppetserver_setup.pp
    Update integration_test.yml
    
    Update integration_test.yml
    
    Update puppetserver plan
    
    Update integration wf
    
    Add missing quote
    
    Update puppetserver_setup.pp

--- plans/puppetserver_setup.pp
+++ plans/puppetserver_setup.pp
@@ -1,9 +1,11 @@
 plan helm::puppetserver_setup(
+  Optional[String] $collection = 'puppet7'
 ) {
   $puppet_server =  get_targets('*').filter |$n| { $n.vars['role'] == 'controller' }
-  $puppet_server_string = $puppet_server[0].name
+  $puppet_server_facts = facts($puppet_server[0])
+  $platform = $puppet_server_facts['platform']
   # install pe server
-  run_task('provision::install_puppetserver', $puppet_server)
+  run_task('provision::install_puppetserver', $puppet_server, { 'collection' => $collection, 'platform' => $platform })
 }
 
 
