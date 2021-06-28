c401425fbeb9a2d31e608c1cd4c8e79ca3ec07c8
daianamezdrea <46529728+daianamezdrea@users.noreply.github.com>
2021-06-28 17:23:23 +0300

    Update chart_spec.rb

--- spec/defines/chart_spec.rb
+++ spec/defines/chart_spec.rb
@@ -13,21 +13,15 @@ describe 'helm::chart', :type => :define do
   let(:params) { {
                  'ensure' => 'present',
                  'path' => [ '/bin','/usr/bin'],
-<<<<<<< HEAD
-                 'release_name' => 'foo'
-=======
                  'release_name' => 'foo',
                  'home' => '/root',
                  'tiller_namespace' => 'kube-system',
                  'kubeconfig' => '/etc/kubernetes/admin.conf',
->>>>>>> f745a94... Add kubeconfig parameter for all defined types to better support Helm v3
+
 
                } }
     it do
       is_expected.to compile.with_all_deps
-<<<<<<< HEAD
-      is_expected.to contain_exec('helm install helm chart')
-=======
       is_expected.to contain_exec('helm install helm chart').with_command("helm install --home '/root' --name 'foo' --tiller-namespace 'kube-system' 'foo'")
     end
 
@@ -38,7 +32,6 @@ describe 'helm::chart', :type => :define do
         is_expected.to compile.with_all_deps
         is_expected.to contain_exec('helm install helm chart').with_command("helm install --kubeconfig '/etc/kubernetes/admin.conf' --name 'foo' 'foo'")
       end
->>>>>>> f745a94... Add kubeconfig parameter for all defined types to better support Helm v3
     end
   end
   context 'with ensure => absent' do
